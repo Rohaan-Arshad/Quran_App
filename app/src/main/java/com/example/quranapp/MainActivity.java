371c9f3f79bec9a953c880984f70465b361a07d8
ibi111 <rohaanarshad15@gmail.com>
2023-06-03 14:48:50 +0500

    2 commit

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.quranapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,134 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list;
+        String[] urduSurahNames = {
+                "الفاتحۃ",
+                " البقرۃ",
+                "آل عمران",
+                "النسآء",
+                "المآئدۃ",
+                "الانعام",
+                "الاعراف",
+                "الانفال",
+                "التوبۃ",
+                "یونس",
+                "ھود",
+                "یوسف",
+                "الرعد",
+                "ابراھیم",
+                "الحجر",
+                "النحل",
+                "الاسراء",
+                "الکہف",
+                "مریم",
+                "طٰہٰ",
+                "الانبیآء",
+                "الحج",
+                "المؤمنون",
+                "النور",
+                "الفرقان",
+                "الشعراء",
+                "النمل",
+                "القصص",
+                "العنکبوت",
+                "الروم",
+                "لقمٰن",
+                "السجدۃ",
+                "الاحزاب",
+                "سبا",
+                "فاطر",
+                "یٰسٓ",
+                "الصّٰفّٰت",
+                "صٓ",
+                "الزمر",
+                "الغافر",
+                "فصلت",
+                "الشُّورٰی",
+                "الزُّخرُف",
+                "الدُّخَان",
+                "الجاثیہ",
+                "الاحقاف",
+                "محمد",
+                "الفتح",
+                "الحجرات",
+                "قٓ",
+                "الذّٰریٰت",
+                "الطّور",
+                "النجم",
+                "القمر",
+                "الرحمٰن",
+                "الواقعۃ",
+                " الحدید",
+                "المجادلۃ",
+                "الحشر",
+                " الممتحنۃ",
+                "الصّف",
+                "الجُمعۃ",
+                "المُنٰفِقُون",
+                " التّغابن",
+                "الطّلاق",
+                "التحریم",
+                "الملک",
+                "القلم",
+                " الحاقہ",
+                "المعارج",
+                "نُوح",
+                "الجن",
+                "المزّمّل",
+                "المدّثّر",
+                "القیٰمۃ",
+                "الانسان",
+                "المرسلٰت",
+                "النَّبَاِ",
+                "النّٰزِعٰتِ",
+                "عَبَسَ",
+                " التَّکوِیر",
+                " الاِنفِطَار",
+                "المُطَفِّفِین",
+                "الاِنشِقَاق",
+                "البُرُوج",
+                "الطَّارق",
+                "الاَعلیٰ",
+                "الغَاشِیَۃ",
+                "الفجر",
+                "البلد",
+                " الشَّمس",
+                "الَّیل",
+                "الضُّحٰی",
+                "الم نشرح",
+                "التّین",
+                "العَلَق",
+                " القدر",
+                "البیّنۃ",
+                "الزّلزال",
+                "العٰدیٰت",
+                " القارعۃ",
+                "التّکاثُر",
+                " العصر",
+                "الھُمَزَۃ",
+                "الفِیل",
+                "قُرَیش",
+                "المَاعُون",
+                "الکوثر",
+                "الکٰفرون",
+                "النَّصَر",
+                "اللَّھب",
+                "الاخلاص",
+                "الفَلَق",
+                " النَّاس"
+        };
+        list = findViewById(R.id.listV);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,urduSurahNames);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 }
